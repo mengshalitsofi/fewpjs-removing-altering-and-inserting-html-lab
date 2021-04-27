65f742a07e7d2aa8ddfed91660a2c0b4ef28632f
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-27 12:29:14 -0700

    commit tree 27f610e8b27fdc4fcf1251ba09480493e2218837

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector("main#main");
+element.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = 'MENGSHA is the champion';
+document.body.appendChild(newHeader);
